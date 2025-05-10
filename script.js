a90ad502e77e0f444054af7b581b8e3e91ea7312
whitmandeb <debbie.whitman01@yahoo.com>
2025-05-09 21:42:59 -0400

    added id to iframe

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let input = document.getElementById("Mood-Selector").value
-console.log(input)
-let button = document.getElementById("button")
+function moodSelector() {
+  let input = document.getElementById("Mood-Selector").value;
+  // create a new variable called iframe and select the iframe by its id
+  let output = document.getElementById("Bodies");
+  let output1 = document.getElementById("happy");
+  if (input === "Happy") {
+    output.classList.toggle("show");
+    output1.classList.remove("show")
+  } else if (input === "Angry") {
+    output1.classList.toggle("show")
+    output.classList.remove("show")
+  }
+}
+console.log(input);
